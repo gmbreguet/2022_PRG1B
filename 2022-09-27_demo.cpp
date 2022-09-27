3d37eae41118d0f0c5ce3b85b9e775dae352d77f
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-09-27 11:33:48 +0200

    2022-09-27 11:33:47 - GMB

---
+++ 2022-09-27_demo.cpp
@@ -0,0 +1,30 @@
+//---------------------------------------------------------
+// Demo           : 2022-09-27_demo
+// Fichier        : 2022-09-27_demo.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// But            : demo en classe
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+   int    entier    = 12;
+   char   car       = 123;
+   string chaine    = "bonjour à jour";
+
+   int    moche     = 'a';
+
+   cin >> car;
+   cout << (int)car;
+   cout <<endl;
+
+   return EXIT_SUCCESS;
+}
