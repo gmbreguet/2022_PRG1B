d97a48d599a47353e9205f6b504c28c359725164
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-10-20 09:31:52 +0200

    2022-10-20 09:31:51 - GMB

---
+++ 2022-10-20_demo.cpp
@@ -0,0 +1,39 @@
+//---------------------------------------------------------
+// Demo           : 2022-10-19_demo
+// Fichier        : 2022-10-19_demo.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// But            : demo en classe
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main () {
+
+   // programme qui transforme en MAJUSCULE une chaine
+   string chaine = "Bonjour a tous";
+
+
+   for (size_t i=0; i<chaine.length(); ++i) {
+      chaine[i] = (char)toupper(chaine[i]);
+   }
+   cout << chaine << endl;
+   chaine = "Bonjour a tous";
+
+   // /!\ & pour etre en lien avec le car de la chaine
+   for (char& c : chaine) {
+      c = (char)toupper(c);
+      cout << c;
+   }
+   cout << endl;
+   cout << chaine << endl;
+
+   return EXIT_SUCCESS;
+}
